fcc630896d45175d1aa72219607178ca7648cffb
Deyu Ming <mingdeyu008@gmail.com>
2021-11-28 14:03:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
 
-VERSION = '2.0-beta'
+VERSION = '2.0-beta1'
 PACKAGE_NAME = 'dgpsi'
 AUTHOR = 'Deyu Ming'
 AUTHOR_EMAIL = 'deyu.ming.16@ucl.ac.uk'
